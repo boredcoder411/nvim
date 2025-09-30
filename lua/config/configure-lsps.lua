6207064c166ae27e79ad0ccd790d27d7d9d63e63
boredcoder411 <taamarcos@gmail.com>
2025-09-30 17:09:20 +0200

    migrate to nvim 0.11+

--- lua/config/configure-lsps.lua
+++ lua/config/configure-lsps.lua
@@ -1,25 +1,19 @@
 require("mason").setup()
 
-local lspconfig = require "lspconfig"
+vim.lsp.enable("clangd")
 
-lspconfig.clangd.setup {}
+vim.lsp.enable("ruff")
 
-lspconfig.ruff.setup {}
-
-lspconfig.lua_ls.setup {
+vim.lsp.config["lua_ls"] = {
   on_init = function(client)
     client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
       runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
         version = "LuaJIT",
       },
-      -- Make the server aware of Neovim runtime files
       workspace = {
         checkThirdParty = false,
         library = {
           vim.env.VIMRUNTIME,
-          -- Depending on the usage, you might want to add additional paths here.
           "${3rd}/luv/library",
           "${3rd}/busted/library",
           vim.api.nvim_get_runtime_file("", true),
@@ -31,8 +25,9 @@ lspconfig.lua_ls.setup {
     Lua = {},
   },
 }
+vim.lsp.enable("lua_ls")
 
-lspconfig.rust_analyzer.setup {
+vim.lsp.config["rust_analyzer"] = {
   settings = {
     ["rust-analyzer"] = {
       diagnostics = {
@@ -41,7 +36,8 @@ lspconfig.rust_analyzer.setup {
     },
   },
 }
+vim.lsp.enable("rust_analyzer")
 
-lspconfig.gopls.setup{}
+vim.lsp.enable("gopls")
 
-lspconfig.svelte.setup {}
+vim.lsp.enable("svelte")
